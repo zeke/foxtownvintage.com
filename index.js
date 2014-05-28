b9d8c2a283f39c06fa172f651029353e5d6674d6
zeke <zeke@sikelianos.com>
2014-05-28 11:41:24 -0400

    nothing

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ require("dotenv").load()
 var express = require("express")
 var harp = require("harp")
 var etsy = require("./lib/etsy").new(process.env.OAUTH_CONSUMER_KEY)
+
+
+var Etsy = require("./lib/etsy")
+var etsy = Etsy.new(process.env.OAUTH_CONSUMER_KEY)
+
 var shop = require("./lib/shop.json")
 var listings = []
 
@@ -13,6 +18,7 @@ app.set('port', (process.env.PORT || 5000))
 app.set('view engine', 'jade')
 
 app.get('/', function(req, res){
+  // res.json(listings)
   res.render('index', {
     listings: listings
   })
